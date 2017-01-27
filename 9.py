a8b2a1df5def2f4265fc4c3bfe7ca915c49574c0
Chen Su <420703660@qq.com>
2017-01-27 17:29:26 +0800

    9.10 - 9.12

--- 9.py
+++ 9.py
@@ -1,23 +1,5 @@
-class Restaurant():
-    """docstring for Restaurant"""
-    def __init__(self, restaurant_name, cuisine_type):
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
-        self.number_served = 0
-
-    def describe_restaurant(self):
-        print("This restaurant's name is " + self.restaurant_name.title() + ".")
-        print("Its cuisine's type is " + self.cuisine_type.title() + ".")
-
-    def open_restaurant(self):
-        print("This restaurant is opening.")
-
-    def set_number_served(self, number_served):
-        self.number_served = number_served
-
-    def increment_number_served(self, increment_number_served):
-        self.number_served += increment_number_served
-
+from restaurant import Restaurant
+from admin import Admin
 
 class IceCreamStand(Restaurant):
     def __init__(self, restaurant_name, cuisine_type, flavors):
@@ -32,44 +14,7 @@ my_icecreamstand = IceCreamStand('chayin', 'ice cream', ['coffee', 'coco', 'gree
 my_icecreamstand.describe_restaurant()
 my_icecreamstand.describe_flavors()
 
-class User():
-    def __init__(self, firstname, lastname, gender, age):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.gender = gender
-        self.age = age
-        self.login_attempts = 0
 
-    def increment_login_attempts(self):
-        self.login_attempts += 1
-
-    def reset_login_attempts(self):
-        self.login_attempts = 0
-
-    def describe_user(self):
-        print("My name is " + self.firstname.title() + self.lastname.title() + ".")
-        print("Gender: " + self.gender.title())
-        print("Age: " + str(self.age))
-        print("Login attempts number: " + str(self.login_attempts))
-
-    def greet_user(self):
-        print("Hi, I'm " + self.firstname.title() + self.lastname.title() + ". Nice to meet you.")
-
-
-class Privileges():
-    def __init__(self):
-        self.privileges = ['can add post', 'can delete post', 'can ban user']
-
-
-    def show_privileges(self):
-        for privilege in self.privileges:
-            print(privilege.title())
-
-
-class Admin(User):
-    def __init__(self, firstname, lastname, gender, age):
-        super().__init__(firstname, lastname, gender, age)
-        self.privileges = Privileges()
 
 test_admin = Admin('Chen', 'Su', 'male', 20)
 test_admin.describe_user()
